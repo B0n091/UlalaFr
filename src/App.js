696b6e53a78c83fbeabe54312a5d2514d7456717
Thomas Bonnaud <thomas.91650@gmail.com>
2019-11-10 02:07:34 +0100

    add material kit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Acceuil from "./Pages/Acceuil"
+
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render () {
+
+    const pages = (
+      <Switch>
+        <Route exact path="/" component={Acceuil}/>
+      </Switch>
+    )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        {pages}
+      </Router>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Pages/Acceuil/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import HeaderPage from '../Header';
+
+class Acceuil extends Component {
+
+  render () {
+
+
+  return (
+    <div>
+      <HeaderPage />
+    </div>
+  );
+}
+}
+
+export default Acceuil;
