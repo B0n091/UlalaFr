71c39c2b5a4bd4ab6790b4c253269044c3e5a57f
Thomas Bonnaud <thomas.91650@gmail.com>
2019-12-01 00:04:01 +0100

    up

--- src/Composants/Familier/details.js
+++ src/Composants/Familier/details.js
@@ -71,7 +71,7 @@ export default function PetDetails(props) {
                     id="classic-modal-slide-description"
                     className={classes.modalBody}
                   >
-                      <img className="petDetails" src={require("../../assets/img/pet/" + `${props.pet.nom}` + "Details.PNG")}  alt={props.pet.nom} />
+                      {/* <img className="petDetails" src={require("../../assets/img/pet/" + `${props.pet.nom}` + "Details.PNG")}  alt={props.pet.nom} /> */}
                     <p>
                      {props.pet.nom}
                     </p>
